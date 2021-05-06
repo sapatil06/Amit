4da3ae65b3b0323a79d565ea4b2b76020c4a6499
amitshete4298 <amitshete9369@gmail.com>
2021-05-06 20:29:41 +0530

    First class by amit added

---
+++ src/packages/Amit.java
@@ -0,0 +1,11 @@
+package packages;
+
+public class Amit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is class by Amit");
+	}
+
+}
