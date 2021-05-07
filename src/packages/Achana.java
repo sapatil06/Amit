edfdbccdaa690c83778ada28eb515a7c580cd6ab
Rchna <archana.malwade@gmail.com>
2021-05-07 19:41:05 +0530

    commited

---
+++ src/packages/Achana.java
@@ -0,0 +1,10 @@
+package packages;
+
+public class Achana {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First class by archana");
+	}
+
+}
