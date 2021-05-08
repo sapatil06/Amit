e6dccc3793c0fde1925348c185d68dfcb0b4a6dd
sapatil06 <sapatil06@gmail.com>
2021-05-08 20:10:05 +0530

    Addition of a+b

--- src/packages/Sangram.java
+++ src/packages/Sangram.java
@@ -4,10 +4,8 @@ public class Sangram {
 
 	public static void main(String[] args) {
 		System.out.println("This is class by sangram");
-		
-		
-		System.out.println("Adding to chrck conflit issue");
-		System.out.println("Line to check conflict");
+		int a=10,b=20;
+		System.out.println("Addition is ="+(a+b));
 	}
 
 }
